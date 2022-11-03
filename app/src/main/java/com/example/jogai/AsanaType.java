fee5a541a7796d165c03f1c31dce978a9eb22de0
jula1717 <julaplocienniczak@gmail.com>
2022-11-03 15:05:28 +0100

    static final numbers for types of asanas add

--- app/src/main/java/com/example/jogai/AsanaType.java
+++ app/src/main/java/com/example/jogai/AsanaType.java
@@ -4,6 +4,15 @@ public class AsanaType {
     String type;
     int id;
 
+    public static final int POZYCJA_STOJACA=1;
+    public static final int POZYCJA_SIEDZACA=2;
+    public static final int SKLON_DO_PRZODU=3;
+    public static final int WYGIECIE_DO_TYLU=4;
+    public static final int SKRET_TULOWIA=5;
+    public static final int POZYCJA_ODWROCONA=6;
+    public static final int POZYCJA_RELAKSACYJNA=7;
+
+
     public AsanaType(String type) {
         this.type = type;
     }
