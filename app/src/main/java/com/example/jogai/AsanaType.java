926654a94ee6b61f570d561e8f1485f492496b38
jula1717 <julaplocienniczak@gmail.com>
2022-11-03 13:01:29 +0100

    AsanaType model

---
+++ app/src/main/java/com/example/jogai/AsanaType.java
@@ -0,0 +1,26 @@
+package com.example.jogai;
+
+public class AsanaType {
+    String type;
+    int id;
+
+    public AsanaType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
