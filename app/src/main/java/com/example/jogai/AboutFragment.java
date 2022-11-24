864301a628a33319bb9d52167b5a639dc0575a1a
jula1717 <julaplocienniczak@gmail.com>
2022-11-24 22:11:11 +0100

    aboutFragment init

---
+++ app/src/main/java/com/example/jogai/AboutFragment.java
@@ -0,0 +1,26 @@
+package com.example.jogai;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+public class AboutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.about,container,false);
+//        TextView textView;
+//        textView = view.findViewById(R.id.textView);
+        return view;
+    }
+}
